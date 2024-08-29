83b3153dc037c235b5a94adafd25b482cff1ff25
Doug Mumme <dougmumme@gmail.com>
2024-08-28 20:00:23 -0500

    updating procfile

--- config/settings/prd.py
+++ config/settings/prd.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS = ['http://stokesdistributing.herokuapp.com/',
                  'stokes-distributing-645656b0c1e9.herokuapp.com/']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # Database
